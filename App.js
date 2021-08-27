3fbd126709023bea7775aff652aa5ebe96ed65fe
Marcelospegiorin <marcelo.mmspe@hotmail.com>
2021-08-27 02:56:09 -0300

    Teste merge

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import {TextInput} from 'react-native'
 
 export default function App() {
 
   const ApiKey = 'RGAPI-b4515564-63e7-4878-aa97-a92b076ea8ee'
   
-  const [sommonerName, setSummonerName] = useState('')
+  const [summonerName, setSummonerName] = useState('')
 
   const getSummoner = async () => {
-    const {data}  = await axios.get(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/srwarriorrj?api_key=${ApiKey}`)
+    const {data}  = await axios.get(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${ApiKey}`)
     console.log(data)
   }
+  console.log(summonerName)
 
   return (
     <View style={styles.container}>
       <Text>LEAGUE API</Text>
       <TextInput 
         onChangeText={(text) => setSummonerName(text)}
+        style={styles.input}
       />
       <Button title="GET SUMMONER" onPress={getSummoner} />
       <StatusBar style="auto" />
@@ -35,4 +35,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 100,
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: '#000000'
+  }
 });
