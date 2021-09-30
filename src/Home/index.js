83e6d6848c55e3a534b1453b279ac774b4d96c9f
Gustavo Monteiro <gustavo.monteiro.guima1@gmail.com>
2021-09-30 17:22:22 -0300

    Update index.js

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Image} from 'react-native'
+import { useNavigation } from '@react-navigation/core';
+import { StatusBar } from 'expo-status-bar'
 
 // ESTILIZAÇÃO
 
@@ -10,32 +12,37 @@ import {
     SubText
 } from './style'
 
-export default function index(navigation) {
-
+export default function Home() {
+        
+    const navigation = useNavigation()
+    
     return (
-
-            <Container>
-                <TitleApp>Guia Of Legends</TitleApp>
-        
-                <Button>
-                    <SubText>Guia</SubText>
-                    <Image source={require('./../../assets/logoLol-icon.png')} style={{width: 50, height: 50, marginLeft: 10}}/>
-                </Button>
-        
-                <Button>
-                    <SubText>O Game</SubText>
-                    <Image source={require('./../../assets/gamepad-icon.png')} style={{width: 50, height: 50, marginLeft: 10}}/>
-                </Button>
-                
-                <Button>
-                    <SubText>Campeões</SubText>
-                    <Image source={require('./../../assets/trofeu-icon.png')} style={{width: 50, height: 50, marginLeft: 10}}/>
-                </Button>
-        
-                <Button>
-                    <SubText>Dicas</SubText>
-                    <Image source={require('./../../assets/tips-icon.png')} style={{width: 50, height: 50, marginLeft: 10}}/>
-                </Button>
-            </Container>
+        <Container >
+            <StatusBar style="light" hidden={true}/>
+            <TitleApp>Guia Of Legends</TitleApp>
+            
+            <Button>
+                <SubText>Guia</SubText>
+                <Image source={require('./../../assets/logoLol-icon.png')} style={{width: 50, height: 50, marginLeft: 10}}/>
+            </Button>
+            
+            <Button>
+                <SubText>O Game</SubText>
+                <Image source={require('./../../assets/gamepad-icon.png')} style={{width: 50, height: 50, marginLeft: 10}}/>
+            </Button>
+                    
+            <Button
+                onPress={() =>
+                navigation.navigate('Campeoes')}
+            >
+                <SubText>Campeões</SubText>
+                <Image source={require('./../../assets/trofeu-icon.png')} style={{width: 50, height: 50, marginLeft: 10}}/>
+            </Button>
+            
+            <Button>
+                <SubText>Dicas</SubText>
+                <Image source={require('./../../assets/tips-icon.png')} style={{width: 50, height: 50, marginLeft: 10}}/>
+            </Button>
+        </Container>
     );
 }
