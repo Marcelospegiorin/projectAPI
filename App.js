cc53a14d97ac81d209a961b865af42eda414acc5
Gustavo Monteiro <gustavo.monteiro.guima1@gmail.com>
2021-09-30 17:53:19 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react';
-import MainStack from './MainStack'
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/Home';
+import Dicas from './src/Campeoes';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-
   return (
-      <NavigationContainer>
-        <MainStack />
-        <StatusBar style="light" hidden={true}/>
-      </NavigationContainer>
-    );
+    <NavigationContainer>
+      <StatusBar style="light" hidden={true}/>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+            headerShown: false,
+        }}
+      >
+          <Stack.Screen
+              name="Home"
+              component={Home}
+          />
+          <Stack.Screen
+              name="Campeoes"
+              component={Campeoes}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
+   );
 }
