a221a632ed04e013c8dcbe258cc05e4bc576d284
Thiago_de_Castro <thiagocd025@gmail.com>
2021-10-25 22:59:14 -0300

    Update index.js

--- src/OGame/index.js
+++ src/OGame/index.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useNavigation } from '@react-navigation/core';
 import { Container, LeagueOfLegends, TextoDoJogo, BlocoHome, TextoHome, TextoGame, Lol } from './style'
+import {ScrollView} from 'react-native';
 
 export default function OGame() {
   
@@ -12,22 +13,29 @@ export default function OGame() {
     }
   
   return (
-    <Container>
+    <Container>           
       <LeagueOfLegends>
         <Lol> League Of Legends</Lol>
       </LeagueOfLegends>
-
-      <TextoGame>
+      <ScrollView> 
+      <TextoGame>      
         <TextoDoJogo>  Consiste em um jogo eletrônico de modo multijogador, onde os jogadores assumem o papel de "summoners" ou invocadores dentre os mais de 140 "campeões", que formarão uma equipe com o objetivo de destruir a base do outro invocador. </TextoDoJogo>
         <TextoDoJogo>  Cada equipe é composta de cinco campeões que começam em nível baixo e progridem de acordo com o desenrolar do jogo através de pontos de experiência e ouro para a compra de itens. </TextoDoJogo>
         <TextoDoJogo> Cada campeão começa a missão com cinco habilidades próprias, e o invocador detém dois feitiços iniciais. A estratégia também é essencial para a utilização dos itens. </TextoDoJogo>
       </TextoGame>
+      <TextoGame>
+        <TextoDoJogo>  Consiste em um jogo eletrônico de modo multijogador, onde os jogadores assumem o papel de "summoners" ou invocadores dentre os mais de 140 "campeões", que formarão uma equipe com o objetivo de destruir a base do outro invocador. </TextoDoJogo>
+        <TextoDoJogo>  Cada equipe é composta de cinco campeões que começam em nível baixo e progridem de acordo com o desenrolar do jogo através de pontos de experiência e ouro para a compra de itens. </TextoDoJogo>
+        <TextoDoJogo> Cada campeão começa a missão com cinco habilidades próprias, e o invocador detém dois feitiços iniciais. A estratégia também é essencial para a utilização dos itens. </TextoDoJogo>        
+      </TextoGame>
+      </ScrollView>
 
       <BlocoHome onPress={() => voltarPagina()}>        
           <TextoHome>Home</TextoHome>        
       </BlocoHome>    
     
-      <StatusBar hidden /> 
+      <StatusBar hidden />
+       
     </Container>
   );
 }
