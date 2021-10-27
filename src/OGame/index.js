44401f8f49d9a22a39e224e821086c835ddcf01b
Thiago_de_Castro <thiagocd025@gmail.com>
2021-10-26 21:54:50 -0300

    Update index.js
    Acrescentando uma View com a imagem do mapa de Lol.

--- src/OGame/index.js
+++ src/OGame/index.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useNavigation } from '@react-navigation/core';
 import { Container, LeagueOfLegends, TextoDoJogo, BlocoHome, TextoHome, TextoGame, Lol } from './style'
-import {ScrollView} from 'react-native';
+import {ScrollView, Image} from 'react-native';
 
 export default function OGame() {
   
@@ -23,11 +23,10 @@ export default function OGame() {
         <TextoDoJogo>  Cada equipe é composta de cinco campeões que começam em nível baixo e progridem de acordo com o desenrolar do jogo através de pontos de experiência e ouro para a compra de itens. </TextoDoJogo>
         <TextoDoJogo> Cada campeão começa a missão com cinco habilidades próprias, e o invocador detém dois feitiços iniciais. A estratégia também é essencial para a utilização dos itens. </TextoDoJogo>
       </TextoGame>
-      <TextoGame>
-        <TextoDoJogo>  Consiste em um jogo eletrônico de modo multijogador, onde os jogadores assumem o papel de "summoners" ou invocadores dentre os mais de 140 "campeões", que formarão uma equipe com o objetivo de destruir a base do outro invocador. </TextoDoJogo>
-        <TextoDoJogo>  Cada equipe é composta de cinco campeões que começam em nível baixo e progridem de acordo com o desenrolar do jogo através de pontos de experiência e ouro para a compra de itens. </TextoDoJogo>
-        <TextoDoJogo> Cada campeão começa a missão com cinco habilidades próprias, e o invocador detém dois feitiços iniciais. A estratégia também é essencial para a utilização dos itens. </TextoDoJogo>        
+      <TextoGame>        
+        <Image source={require('../OGame/lol_minimap_redim.jpg')} />              
       </TextoGame>
+        
       </ScrollView>
 
       <BlocoHome onPress={() => voltarPagina()}>        
