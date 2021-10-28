8ee678a82fe95e2a330a7dfdc842c30f5dca44b4
Gustavo Monteiro <gustavo.monteiro.guima1@gmail.com>
2021-10-28 00:00:35 -0300

    css att

--- src/Dicas/style.js
+++ src/Dicas/style.js
@@ -1,44 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.View`
-    width: 100%
-    height: 100%
-    align-items: center
-    backgroundColor: #011425
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    background-color: #011425;
 `;
 
 export const Button = styled.TouchableOpacity`
-    width: 250px
-    height: 100px
+    width: 250px;
+    height: 100px;
 
-    margin-top: 20px
+    margin-top: 20px;
 
-    align-items: center
-    justify-content: center
+    align-items: center;
+    justify-content: center;
 
-    backgroundColor: #063C6A
+    background-color: #063C6A;
 
-    borderColor: #1ECEE7
-    borderWidth: 2px
-    border-radius: 15px
+    border-color: #1ECEE7;
+    border-width: 2px;
+    border-radius: 15px;
 
-    flex-direction: row
+    flex-direction: row;
 `;
 
 export const TitleApp = styled.Text`
-    font-size: 40px
-    color: #CAA92E
-    margin-top: 50px
+    font-size: 40px;
+    color: #CAA92E;
+    margin-top: 50px;
 `;
 
 export const SubText = styled.Text`
-    font-size: 30px
-    color: #CAA92E
+    font-size: 30px;
+    color: #CAA92E;
 `;
 
 export const SubView = styled.View`
-    width: 100%
-    height: 200px
-    align-items: center
-    margin-top: 50px
+    width: 100%;
+    height: 200px;
+    align-items: center;
+    margin-top: 50px;
 `;
--- src/Itens/style.js
+++ src/Itens/style.js
@@ -1,45 +1,45 @@
 import styled from "styled-components";
 
 export const Container = styled.View`
-    width: 100%
-    height: 100%
-    align-items: center
-    backgroundColor: #011425
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    background-color: #011425;
 `;
 
 export const Button = styled.TouchableOpacity`
-    width: 125px
-    height: 50px
-    margin-top: 10px
-    align-items: center
-    margin-bottom: 10px
-    justify-content: center
-    backgroundColor: #063C6A
-    borderColor: #1ECEE7
-    borderWidth: 2px
-    border-radius: 15px
-    flex-direction: row
+    width: 125px;
+    height: 50px;
+    margin-top: 10px;
+    align-items: center;
+    margin-bottom: 10px;
+    justify-content: center;
+    background-color: #063C6A;
+    border-color: #1ECEE7;
+    border-width: 2px;
+    border-radius: 15px;
+    flex-direction: row;
 `;
 
 export const ViewTitulo = styled.View`
-    align-items: center
-    justify-content: center
-    borderColor: #1ECEE7
-    borderWidth: 2px
-    border-radius: 7px
-    backgroundColor: #063C6A
-    margin-top: 15px
-    margin-bottom: 20px
-    width: 90%
-    height: 60px
+    align-items: center;
+    justify-content: center;
+    border-color: #1ECEE7;
+    border-width: 2px;
+    border-radius: 7px;
+    background-color: #063C6A;
+    margin-top: 15px;
+    margin-bottom: 20px;
+    width: 90%;
+    height: 60px;
 `;
 
 export const TituloItens = styled.Text`
-    font-size: 33px
-    color: #CAA92E
+    font-size: 33px;
+    color: #CAA92E;
 `;
 
 export const TextButton = styled.Text`
-    font-size: 20px
-    color: #CAA92E
+    font-size: 20px;
+    color: #CAA92E;
 `;
