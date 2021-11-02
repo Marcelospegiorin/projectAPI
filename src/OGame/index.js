0070a6f9a2bec61211762f87d81cf0e08118bde2
Thiago_de_Castro <thiagocd025@gmail.com>
2021-11-02 18:24:12 -0300

    Update index.js

--- src/OGame/index.js
+++ src/OGame/index.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useNavigation } from '@react-navigation/core';
-import { Container, LeagueOfLegends, TextoDoJogo, BlocoHome, TextoHome, TextoGame, Lol } from './style'
+import { Container, LeagueOfLegends, TextoDoJogo, BlocoHome, TextoHome, TextoGame, BlocoImagem, Lol } from './style'
 import {ScrollView, Image} from 'react-native';
 
 export default function OGame() {
@@ -17,18 +17,19 @@ export default function OGame() {
       <LeagueOfLegends>
         <Lol> League Of Legends</Lol>
       </LeagueOfLegends>
+      
       <ScrollView> 
         <TextoGame>      
           <TextoDoJogo>  Consiste em um jogo eletrônico de modo multijogador, onde os jogadores assumem o papel de "summoners" ou invocadores dentre os mais de 140 "campeões", que formarão uma equipe com o objetivo de destruir a base do outro invocador. </TextoDoJogo>
           <TextoDoJogo>  Cada equipe é composta de cinco campeões que começam em nível baixo e progridem de acordo com o desenrolar do jogo através de pontos de experiência e ouro para a compra de itens. </TextoDoJogo>
           <TextoDoJogo> Cada campeão começa a missão com cinco habilidades próprias, e o invocador detém dois feitiços iniciais. A estratégia também é essencial para a utilização dos itens. </TextoDoJogo>
         </TextoGame>
-        <TextoGame>        
-          <Image source={require('../OGame/lol_minimap_redim.jpg')}/>                      
-        </TextoGame>
-                
+        <BlocoImagem style={{flex:1}}>        
+          <Image style={{width:388, height:360}} source={require('../OGame/lol_minimap_redim.jpg')}        
+          />                      
+        </BlocoImagem> 
       </ScrollView>
-
+      
       <BlocoHome onPress={() => voltarPagina()}>        
           <TextoHome>Home</TextoHome>        
       </BlocoHome>    
