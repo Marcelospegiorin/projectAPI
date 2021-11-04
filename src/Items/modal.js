4d98f8a739d46222f38e986a555e6d8de8889b3c
Gustavo Monteiro <gustavo.monteiro.guima1@gmail.com>
2021-11-03 22:18:23 -0300

    Update modal.js

--- src/Items/modal.js
+++ src/Items/modal.js
@@ -4,15 +4,15 @@ export const Modal = styled.Modal`
 `;
 
 export const ModalContent = styled.View`
-    flex: 1
-    justify-content: flex-end
+    flex: 1;
+    justify-content: flex-end;
 `;
 
 export const ContainerModal = styled.View`
-    border-top-left-radius: 20px
-    border-top-right-radius: 10px
-    padding: 10px 20px 40px 20px
-    background-color: blue
-    min-height: 430px
-    align-items: center
+    border-top-left-radius: 20px;
+    border-top-right-radius: 10px;
+    padding: 10px 20px 40px 20px;
+    background-color: blue;
+    min-height: 430px;
+    align-items: center;
 `;
