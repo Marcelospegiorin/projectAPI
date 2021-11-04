0445213132905d9112a5e675924ba6503a3fef9b
Gustavo Monteiro <gustavo.monteiro.guima1@gmail.com>
2021-11-04 19:26:52 -0300

    att runas

--- src/Runas/index.js
+++ src/Runas/index.js
@@ -1,32 +1,105 @@
-import React from 'react'
-import { ScrollView } from 'react-native';
-import { Container, Button, ViewTitulo, TituloRunas, TextButton } from './style'
-import { useNavigation } from '@react-navigation/core';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigation } from '@react-navigation/core';
+import { ActivityIndicator } from 'react-native-paper';
+
+import {
+  Container,
+  Title,
+  WrapScroll,
+  WrapItem,
+  ItemImage,
+  ItemName,
+  NomeRuna,
+  ViewNameItem,
+  ViewImage,
+  ContainerLoading,
+} from './style';
+
+import {
+  Modal,
+  ModalContent,
+  ContainerModal
+} from './modal';
 
 export default function Runas() {
+  const [items, setItems] = useState([]);
 
-    const navigation = useNavigation()
+  const navigation = useNavigation();
 
-    function voltarPagina(){
-        navigation.goBack()
+  const [loading ,setLoading] = useState(true)
+
+  const [modalIsVisible, setModalIsVisible] = useState(false)
+
+  useEffect(() => {
+    async function loadItems() {
+      const { data } = await axios.get(
+        'http://ddragon.leagueoflegends.com/cdn/11.22.1/data/pt_BR/item.json'
+      );
+      setLoading(false)
+      setItems(await Object.entries(data.data));
     }
+    loadItems();
+  }, []);
 
-    return (
-        <Container>
-            <ViewTitulo>
-                <TituloRunas>Runas</TituloRunas>
-            </ViewTitulo>
-            <ScrollView
-                showsHorizontalScrollIndicator={false}
-            >
-                
-            </ScrollView>
-            <Button
-                onPress={() => voltarPagina()}
-            >
-                <TextButton>Home</TextButton>
-            </Button>
-        </Container>
-    );
+  const [itemSelected, setItemSelected] = useState([
+    {teste: ''},
+    {name: ''},
+    {gold: ''},
+    {plaintext: ''}
+  ])
+
+  console.log(itemSelected)
+
+  function OpenModal(item) {
+    setItemSelected(item)
+    setModalIsVisible(true)
+  }
+
+  return (
+    <Container>
+      <Title>Itens</Title>
+      {loading == true
+      ?
+        <ContainerLoading>
+         <ActivityIndicator size={100} color='#CAA92E' animating={true}/>
+        </ContainerLoading>
+      :
+        <WrapScroll>
+
+          {items.map(item => (
+            <WrapItem onPress={() => OpenModal(item)}>
+              <ViewImage>
+                <ItemImage
+                  source={{
+                    uri: `http://ddragon.leagueoflegends.com/cdn/11.22.1/img/item/${item[1].image.full}`,
+                  }}
+                />
+              </ViewImage>
+            
+              <ViewNameItem>
+                <ItemName>{item[1].name}</ItemName>
+              </ViewNameItem>
+            </WrapItem>
+          ))}
+        </WrapScroll>
+      }
+
+      <Modal 
+        animationType="slide"
+        visible={modalIsVisible}
+        transparent={true}
+        onRequestClose={() => setModalIsVisible(false)}
+      >
+        <ModalContent>
+          <ContainerModal>
+            <NomeRuna>Nome do item: {itemSelected[1].name}</NomeRuna>
+            <ItemName>Preço total: {itemSelected[1].gold.total}</ItemName>
+            <ItemName>Descrição: {itemSelected[1].plaintext}</ItemName>
+          </ContainerModal>
+        </ModalContent>
+      </Modal>
+
+    </Container>
+  );
 }
